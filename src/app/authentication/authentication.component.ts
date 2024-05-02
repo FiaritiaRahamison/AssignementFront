ee95fb23f9a40ab3e44a38f01d72e35675e5e74d
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-05-02 16:55:17 +0300

    login

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-authentication',
   standalone: true,
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule,],
+  imports: [MatCardModule, MatFormFieldModule,
+            MatInputModule, MatIconModule,
+            MatButtonModule, FormsModule],
   templateUrl: './authentication.component.html',
   styleUrl: './authentication.component.css'
 })
 export class AuthenticationComponent {
+  hide = true;
 
+  username = '';
+  password = '';
+  errorMessage = '';
+
+  onSubmit(event: any) {
+    if((this.username == '') || (this.password == '')) {
+      this.errorMessage = 'The username and the password are required';
+    } else {
+      console.log("Hey ");
+    }
+  }
 }
