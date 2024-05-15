9e500c2d9c862a3d301dbf40cfc3f4d926d78769
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-05-15 12:20:50 +0300

    component liste assignment teacher

---
+++ src/app/assignments/assignment-list-teacher/assignment-list-teacher.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-list-teacher',
+  standalone: true,
+  imports: [],
+  templateUrl: './assignment-list-teacher.component.html',
+  styleUrl: './assignment-list-teacher.component.css'
+})
+export class AssignmentListTeacherComponent {
+
+}
