a285e1e71df20da070de3c65bb65ce7e0ad6c78c
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-05-21 09:48:23 +0300

    debut liste assignment admin

---
+++ src/app/assignments/assignment-list-admin/assignment-list-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-list-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './assignment-list-admin.component.html',
+  styleUrl: './assignment-list-admin.component.css'
+})
+export class AssignmentListAdminComponent {
+
+}
