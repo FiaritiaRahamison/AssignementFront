b5e0571af09ec7a0aa6f3b83499d6816b75006a3
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-05-25 00:18:27 +0300

    edit sidebar

--- src/app/layouts/sidebar/sidebar.component.ts
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -7,8 +7,9 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import {MatListModule, MatNavList} from '@angular/material/list';
-
-
+import { Router } from '@angular/router';
+import { User } from '../../models/token';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
@@ -22,19 +23,39 @@ import {MatListModule, MatNavList} from '@angular/material/list';
     MatButtonModule,
     MatInputModule,
     MatNavList,
-    MatListModule
+    MatListModule,
+    CommonModule
   ],
   templateUrl: 'sidebar.component.html',
   styleUrl: 'sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   options = this._formBuilder.group({
     bottom: 0,
     fixed: false,
     top: 0,
   });
 
-  constructor(private _formBuilder: FormBuilder) {}
+  userConnected!: User;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private router : Router
+  ) {}
+
+  ngOnInit(): void {
+    let dataUser = window.localStorage.getItem("user");
+    let dataToken = window.localStorage.getItem("token");
+    if(dataUser && dataToken) {
+      this.userConnected = JSON.parse(dataUser);
+    }
+  }
 
   shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(window.location.host);
+
+  goToAssignmentList() {
+    if(this.userConnected.role == 1) this.router.navigate(['/student/assignments']);
+    if(this.userConnected.role == 2) this.router.navigate(['/teacher/assignments']);
+    if(this.userConnected.role == 3) this.router.navigate(['/admin/assignments']);
+  }
 }
