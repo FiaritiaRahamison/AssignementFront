f907d4279a2706afce0c9bc4a636f420f3dbf054
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-05-28 19:24:55 +0300

    css add subject sans upload image

--- src/app/subject/add-subject/add-subject.component.ts
+++ src/app/subject/add-subject/add-subject.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/token';
+import { TitleService } from '../../shared/title.service';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-add-subject',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatInputModule, MatFormFieldModule,
+    MatButtonModule, MatSelectModule
+  ],
   templateUrl: './add-subject.component.html',
   styleUrl: './add-subject.component.css'
 })
-export class AddSubjectComponent {
+export class AddSubjectComponent implements OnInit {
+
+  userConnected!: User;
+
+  constructor(
+    private titleService: TitleService
+  ){}
+
+  ngOnInit(): void {
+    let data = window.localStorage.getItem("user");
+    if (data) {
+      this.userConnected = JSON.parse(data);
+    } else {
+      console.log("Aucune donnée utilisateur trouvée dans le localStorage.");
+    }
+
+    if(this.userConnected) {
+      this.titleService.changeTitle(`Add a subject`);
+    }
+  }
 
 }
