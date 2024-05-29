cd522f1b18112ea315bcc061e1654e340864e71a
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-05-29 10:46:48 +0300

    list subject admin

--- src/app/shared/subject-service.service.ts
+++ src/app/shared/subject-service.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Server } from '../environment/server';
 import { Observable } from 'rxjs';
 import { Subject } from '../models/subject';
+import { catchError, map, tap } from 'rxjs/operators';
+import { ApiResponse } from '../models/api-response';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +19,16 @@ export class SubjectServiceService {
   getSubjects(page: number, limit: number): Observable<any> {
     const urlSubject = `${this.server.getUrl()}/api/subjects?page=${page}&limit=${limit}`;
 
-    return this.http.get<Subject[]>(urlSubject);
+    const bearerToken = localStorage.getItem('token');
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${bearerToken}`
+    });
+
+    return this.http.get<ApiResponse>(urlSubject, { headers }).pipe(
+      map((response) => response.data),
+      tap((data: Subject[]) => {
+      })
+    );
   }
 }
